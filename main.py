40bd344e7eca27c7a1c6765dc72721f855d34092
えんぴつ <140484956+HB-empitsu@users.noreply.github.com>
2024-08-31 21:22:48 +0900

    Update main.py
    全体調整

--- main.py
+++ main.py
@@ -13,13 +13,87 @@ import folium.plugins
 import streamlit as st
 
 
-def fetch_soup(url, parser="html.parser"):
+def fetch_soup(url, parser="html5lib"):
     r = requests.get(url)
     r.raise_for_status()
 
     soup = BeautifulSoup(r.content, parser)
 
     return soup
+
+
+def fetch_hinanjo(url):
+    soup = fetch_soup(url)
+
+    tag = soup.find(string=re.compile(r"^補足情報"))
+    information = (
+        unicodedata.normalize("NFKC", tag.parent.get_text(strip=True)).replace("。", "。  \n").strip() if tag else ""
+    )
+
+    data = []
+
+    for tr in soup.select("table.listViewTable > tbody > tr"):
+        tds = tr.select("td")
+
+        if len(tds) == 8:
+            td = [i.get_text(strip=True) for i in tds]
+
+            link = tds[3].select_one("a")
+
+            text = link.get("onclick") or link.get("href")
+
+            pattern = r"lat=([0-9.]+)&lng=([0-9.]+)"
+            match = re.search(pattern, text)
+
+            if match:
+                lat = match.group(1)
+                lng = match.group(2)
+
+                td.append(lat)
+                td.append(lng)
+            else:
+                print("No match found")
+
+            data.append(td)
+
+    if data:
+        df = pd.DataFrame(
+            data,
+            columns=[
+                "避難所名",
+                "開設状況",
+                "所在地",
+                "地図",
+                "電話番号",
+                "収容人数",
+                "避難世帯数",
+                "避難人数",
+                "緯度",
+                "経度",
+            ],
+        )
+
+        df = df.drop("地図", axis=1).astype({"収容人数": int, "緯度": float, "経度": float})
+
+        df["避難世帯数"] = pd.to_numeric(df["避難世帯数"], errors="coerce").fillna(0).astype(int)
+        df["避難人数"] = pd.to_numeric(df["避難人数"], errors="coerce").fillna(0).astype(int)
+
+    else:
+        df = pd.DataFrame(
+            columns=[
+                "避難所名",
+                "開設状況",
+                "収容人数",
+                "避難世帯数",
+                "避難人数",
+                "緯度",
+                "経度",
+                "所在地",
+                "電話番号",
+            ]
+        )
+
+    return df, information
 
 
 def set_color(value, max_value):
@@ -57,66 +131,7 @@ def load_data():
 
     status = "".join(dt.split())
 
-    soup = fetch_soup(link)
-
-    temp = soup.find(string=re.compile(r"^補足情報"))
-
-    information = ""
-
-    if temp:
-        information = unicodedata.normalize("NFKC", temp.parent.get_text(strip=True)).replace("。", "。  \n").strip()
-
-    data = []
-
-    for tr in soup.select("table.listViewTable > tr")[2:-1]:
-        tds = tr.select("td")
-
-        if len(tds) == 8:
-            td = [i.get_text(strip=True) for i in tds]
-    
-            text = tds[3].select_one("a").get("onclick")
-    
-            pattern = r"lat=([0-9.]+)&lng=([0-9.]+)"
-    
-            match = re.search(pattern, text)
-    
-            if match:
-                lat = match.group(1)
-                lng = match.group(2)
-    
-                td.append(lat)
-                td.append(lng)
-            else:
-                print("No match found")
-    
-            data.append(td)
-
-    df = (
-        pd.DataFrame(
-            data,
-            columns=[
-                "避難所名",
-                "開設状況",
-                "所在地",
-                "地図",
-                "電話番号",
-                "収容人数",
-                "避難世帯数",
-                "避難人数",
-                "緯度",
-                "経度",
-            ],
-        )
-        .drop("地図", axis=1)
-        .astype({"収容人数": int, "緯度": float, "経度": float})
-    )
-
-    df["避難世帯数"] = pd.to_numeric(df["避難世帯数"], errors="coerce").fillna(0).astype(int)
-    df["避難人数"] = pd.to_numeric(df["避難人数"], errors="coerce").fillna(0).astype(int)
-
-    df["navi"] = df.apply(
-        lambda x: f'https://www.google.com/maps/dir/?api=1&destination={x["緯度"]},{x["経度"]}', axis=1
-    )
+    df, information = fetch_hinanjo(link)
 
     return df, title, status, date, link, information
 
@@ -131,12 +146,14 @@ st.write("[避難所 詳細情報](%s)" % link)
 st.subheader(f"{date} {status}")
 st.write(f"{information}")
 
-if df0.empty:
+if not df0.empty:
+    st.write(
+        "開設数：", (df0["開設状況"] == "開設").sum(), "施設", "、閉鎖数：", (df0["開設状況"] == "閉鎖").sum(), "施設"
+    )
+    st.write("避難人数：", df0["避難人数"].sum(), "人、", "避難世帯数：", df0["避難世帯数"].sum(), "世帯")
 
-    st.write("避難人数：", df0["避難人数"].sum(), "人、", "避難世帯数：", df0["避難世帯数"].sum(), "世帯、", "開設数：", (df0["開設状況"] == "開設").sum())
-    
     lat, lng = 34.0663183, 132.997528
-    
+
     # フォリウムマップの初期化
     m = folium.Map(
         location=[lat, lng],
@@ -144,14 +161,18 @@ if df0.empty:
         attr='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>',
         zoom_start=14,
     )
-    
+
+    df0["navi"] = df0.apply(
+        lambda x: f'https://www.google.com/maps/dir/?api=1&destination={x["緯度"]},{x["経度"]}', axis=1
+    )
+
     # データフレームからマーカーを追加
     for _, row in df0.iterrows():
         color = set_color(row["避難人数"], row["収容人数"])
-    
+
         if row["開設状況"] != "開設":
             color = "gray"
-    
+
         folium.Marker(
             location=[row["緯度"], row["経度"]],
             popup=folium.Popup(
@@ -161,24 +182,23 @@ if df0.empty:
             tooltip=row["避難所名"],
             icon=folium.Icon(color=color),
         ).add_to(m)
-    
+
     # 現在値
     folium.plugins.LocateControl().add_to(m)
-    
-    
+
     # マップをストリームリットに表示
     st_data = st_folium(m, width=500, height=300)
-    
+
     # マップ境界内のデータフィルタリングと距離計算
     if st_data:
         bounds = st_data["bounds"]
         center = st_data.get("center", {"lat": lat, "lng": lng})
-    
+
         southWest_lat = bounds["_southWest"]["lat"]
         southWest_lng = bounds["_southWest"]["lng"]
         northEast_lat = bounds["_northEast"]["lat"]
         northEast_lng = bounds["_northEast"]["lng"]
-    
+
         # 境界内のポイントをフィルタリング
         filtered_df = df0.loc[
             (df0["緯度"] >= southWest_lat)
@@ -186,16 +206,16 @@ if df0.empty:
             & (df0["経度"] >= southWest_lng)
             & (df0["経度"] <= northEast_lng)
         ].copy()
-    
+
         # 距離計算
         grs80 = Geod(ellps="GRS80")
         filtered_df["distance"] = filtered_df.apply(
             lambda row: grs80.inv(center["lng"], center["lat"], row["経度"], row["緯度"])[2], axis=1
         )
-    
+
         # 距離でソート
         filtered_df.sort_values("distance", inplace=True)
-    
+
         # 結果を表示
         df1 = (
             filtered_df[
@@ -221,4 +241,4 @@ if df0.empty:
             hide_index=True,
         )
 else:
-    st.write("データなし")
+    st.write("避難所なし")
