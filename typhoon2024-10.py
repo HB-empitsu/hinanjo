afb0ea515301dd6a214860eadb71f320a14baee1
えんぴつ <140484956+HB-empitsu@users.noreply.github.com>
2024-09-01 10:30:08 +0900

    Update typhoon2024-10.py

--- typhoon2024-10.py
+++ typhoon2024-10.py
@@ -1,44 +1,10 @@
 import math
-from urllib.parse import urljoin
 
 import pandas as pd
 import plotly.graph_objects as go
-import requests
-from bs4 import BeautifulSoup
 from plotly.subplots import make_subplots
 
 import streamlit as st
-
-
-def fetch_soup(url, parser="html5lib"):
-    r = requests.get(url)
-    r.raise_for_status()
-
-    soup = BeautifulSoup(r.content, parser)
-
-    return soup
-
-
-def fetch_hinanjo(url, ts_date):
-    soup = fetch_soup(url)
-
-    table = soup.select_one("table.listViewTable")
-
-    if table:
-        df = pd.read_html(url, skiprows=1, header=0, attrs={"class": "listViewTable"}, flavor="html5lib")[0]
-        df = df[df["所在地.1"] == "地図"].drop("所在地.1", axis=1)
-
-        df["収容 人数"] = df["収容 人数"].fillna(0).astype(int)
-        df["避難 世帯数"] = df["避難 世帯数"].fillna(0).astype(int)
-        df["避難 人数"] = df["避難 人数"].fillna(0).astype(int)
-
-        df["日付"] = ts_date
-
-    else:
-        df = pd.DataFrame()
-
-    return df
-
 
 st.set_page_config(
     page_title="今治市 避難所情報", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None
@@ -47,46 +13,17 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    url = "https://city-imabari.my.salesforce-sites.com/K_PUB_VF_HinanjyoList"
-
-    soup = fetch_soup(url)
-
-    dfs = []
-    data = []
-
-    for tag in soup.select("div.volunteer > dl")[::-1]:
-        dt = tag.select_one("dt").get_text(strip=True)
-        date, title = [i.get_text(strip=True) for i in tag.select("dd > p")]
-
-        title = title.replace("今治市 避難所情報 :", "")
-        status = "".join(dt.split())
-
-        link = urljoin(url, tag.select_one("a").get("href"))
-
-        ts_date = pd.to_datetime(date, errors="coerce")
-
-        d = {"title": title, "status": status, "date": ts_date, "link": link}
-        data.append(d)
-
-        df = fetch_hinanjo(link, ts_date)
-
-        if not df.empty:
-            dfs.append(df)
-
-    df_info = pd.DataFrame(data).set_index("date", drop=True)
-
-    df_data = pd.concat(dfs, ignore_index=True)
+    df_info = pd.read_csv("info.csv", index_col="date", parse_dates=True)
+    df_data = pd.read_csv("data.csv", parse_dates=["日付"])
 
     return df_info, df_data
 
 
 df_info, df_data = load_data()
 
-# gdf.get_group("2024-08-31 09:59:00")
+df0 = df_data[df_data["避難人数"] > 0].copy()
 
-df0 = df_data[df_data["避難 人数"] > 0].copy()
-
-pv = df0.pivot(index="日付", columns="避難所名", values="避難 人数").reindex(index=df_info.index).fillna(0).astype(int)
+pv = df0.pivot(index="日付", columns="避難所名", values="避難人数").reindex(index=df_info.index).fillna(0).astype(int)
 df1 = pv.assign(合計=pv.sum(axis=1)).copy()
 
 df2 = pv.diff().fillna(pv).astype(int).copy()
